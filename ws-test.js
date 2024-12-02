e949224163b1c27e52c6b96b26c55d4aa5969902
Tejaswi Gowda <tejaswi@asu.edu>
2024-12-02 15:01:39 -0700

    ac

--- ws-test.js
+++ ws-test.js
@@ -1,9 +1,30 @@
+// node osc
+
+const osc = require('osc');
+const udpPort = new osc.UDPPort({
+    localAddress: "127.0.0.1",
+    localPort: 6666,
+    metadata: true
+});
+
+udpPort.open();
+
+
 
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8081 });
+const wss = new WebSocket.Server({ port: 5555 });
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
+        udpPort.send({
+            address: "/message",
+            args: [
+                {
+                    type: "s",
+                    value: message
+                }
+            ]
+        });
     });
 });
