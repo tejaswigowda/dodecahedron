67a538a7252125b6a3eacde6c58324b6519f2742
Tejaswi Gowda <tejaswi@asu.edu>
2024-12-02 15:15:31 -0700

    ac

--- ws-test.js
+++ ws-test.js
@@ -1,15 +1,23 @@
-// node osc
-var Client = require('node-rest-client').Client;
-var client = new Client();
 
+var http = require('http');
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 5555 });
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
-        client.get("http://localhost:8080/position?msg=" + message, function (data, response) {
-            console.log(data);
+        // make http get
+        http.get('http://localhost:8080?message=' + message
+            , (res) => {
+            console.log('statusCode:', res.statusCode);
+            console.log('headers:', res.headers);
+
+            res.on('data', (d) => {
+                process.stdout.write(d);
+            });
+        }).on('error', (e) => {
+            console.error(e);
         });
+
     });
 });
